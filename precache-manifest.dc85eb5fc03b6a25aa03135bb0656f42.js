289a2514d4d1d0ae379e1f6bee4a84206bf91a19
markoffkin <30995929+MarkMarkovkin@users.noreply.github.com>
2019-05-01 01:30:15 +0500

    Updates

---
+++ precache-manifest.dc85eb5fc03b6a25aa03135bb0656f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4648bb9205c0e5f395df",
+    "url": "/wendypendy/static/css/main.d895fae4.chunk.css"
+  },
+  {
+    "revision": "4648bb9205c0e5f395df",
+    "url": "/wendypendy/static/js/main.4648bb92.chunk.js"
+  },
+  {
+    "revision": "e515a3bfe173a8fe41d3",
+    "url": "/wendypendy/static/js/runtime~main.e515a3bf.js"
+  },
+  {
+    "revision": "e7fa6bb962f7af8b55f3",
+    "url": "/wendypendy/static/css/2.e6321a10.chunk.css"
+  },
+  {
+    "revision": "e7fa6bb962f7af8b55f3",
+    "url": "/wendypendy/static/js/2.e7fa6bb9.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/wendypendy/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "db317d223f3d0b2496bd14ed40236576",
+    "url": "/wendypendy/index.html"
+  }
+];
